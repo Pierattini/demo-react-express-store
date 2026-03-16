9f88eb3d7dbbbfecb908525ca6ed065a1d16212b
carlo <carloantonio.pierattini@udec.cl>
2026-03-16 21:07:07 +0100

    fix render port

--- frontend/src/components/layout/Container.tsx
+++ frontend/src/components/layout/Container.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default function Container({ children, className = "" }: Props) {
   return (
-    <div className={`mx-auto w-full max-w-[1400px] px-6 ${className}`}>
+    <div className={`mx-auto w-full px-6 ${className}`}>
       {children}
     </div>
   );
