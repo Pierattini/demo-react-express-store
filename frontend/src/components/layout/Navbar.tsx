31fa5e786c18265c27ec34ebba53e3915509d382
carlo <carloantonio.pierattini@udec.cl>
2026-03-16 14:41:08 +0100

    fix render port

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -212,7 +212,7 @@ export default function Navbar() {
     {/* menu */}
     <div
       className="
-      absolute top-[72px] left-6
+      absolute top-[72px] left-6 w-[220px]
       bg-white/90 backdrop-blur-md
       shadow-xl shadow-black/10
       border border-gray-200
