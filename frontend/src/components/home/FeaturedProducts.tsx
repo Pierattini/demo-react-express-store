87fc3d3f24f490c3fd4917e65df6e01e5e288337
carlo <carloantonio.pierattini@udec.cl>
2026-03-16 16:57:37 +0100

    fix render port

--- frontend/src/components/home/FeaturedProducts.tsx
+++ frontend/src/components/home/FeaturedProducts.tsx
@@ -91,7 +91,7 @@ export default function FeaturedProducts() {
         </div>
 
         {/* Carousel */}
-        <div className="relative flex items-center px-12">
+        <div className="relative flex items-center">
 
           {startIndex > 0 && (
             <button
